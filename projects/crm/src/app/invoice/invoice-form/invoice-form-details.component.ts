db957549683c06d388e0dca15db3410cecf190db
h-manuel-sekoia <h.manuel@sekoia.ch>
2023-03-23 10:58:34 +0100

    Implementation of the InvoiceFormDetails component

--- projects/crm/src/app/invoice/invoice-form/invoice-form-details.component.ts
+++ projects/crm/src/app/invoice/invoice-form/invoice-form-details.component.ts
@@ -1,20 +1,118 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-form-details',
   template: `
-    <p>
-      invoice-form-details works!
-    </p>
+    <ng-container [formGroup]="parent" *ngIf="parent && details">
+      <div class="alert bg-warning text-white" *ngIf="details.length === 0">
+        <p>Vous devez ajouter des détails à votre facture</p>
+        <button type="button" class="btn btn-sm btn-outline-light" (click)="detailsAddedEvent.emit()">
+          + Ajouter ma première ligne
+        </button>
+      </div>
+      <section formArrayName="details">
+        <div
+          class="detail-row"
+          *ngFor="let group of details.controls; let i = index"
+          [formGroup]="group"
+        >
+          <div class="row mb-3">
+            <div class="col-7">
+              <input
+                formControlName="description"
+                [class.is-invalid]="
+                  group.controls.description.touched &&
+                  group.controls.description.invalid
+                "
+                name="description_{{ i }}"
+                id="description_{{ i }}"
+                type="text"
+                placeholder="Description"
+                class="form-control"
+              />
+              <p class="invalid-feedback">
+                La description est obligatoire et doit faire au moins 5
+                caractères !
+              </p>
+            </div>
+            <div class="col-2">
+              <input
+                formControlName="amount"
+                [class.is-invalid]="
+                  group.controls.amount.touched && group.controls.amount.invalid
+                "
+                name="amount_{{ i }}"
+                id="amount_{{ i }}"
+                type="number"
+                placeholder="Montant"
+                class="form-control"
+              />
+              <p class="invalid-feedback">Le montant est obligatoire</p>
+            </div>
+            <div class="col-2">
+              <input
+                formControlName="quantity"
+                [class.is-invalid]="
+                  group.controls.quantity.touched &&
+                  group.controls.quantity.invalid
+                "
+                type="number"
+                placeholder="Quantité"
+                class="form-control"
+              />
+              <p class="invalid-feedback">La quantité est obligatoire</p>
+            </div>
+            <div class="col-1">
+              <button
+                type="button"
+                class="btn w-auto d-block btn-sm btn-danger"
+                (click)="detailsRemovedEvent.emit(i)"
+              >
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+          *ngIf="details.length > 0"
+          class="btn btn-primary btn-sm"
+          type="button"
+          (click)="detailsAddedEvent.emit()"
+        >
+          + Ajouter une ligne
+        </button>
+      </section>
+    </ng-container>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class InvoiceFormDetailsComponent implements OnInit {
+  @Output('details-removded') 
+  detailsRemovedEvent = new EventEmitter<number>();
 
-  constructor() { }
+  @Output('details-added')
+  detailsAddedEvent = new EventEmitter();
 
-  ngOnInit(): void {
+  @Input()
+  parent?: FormGroup<{
+    customer_name: FormControl;
+    description: FormControl;
+    status: FormControl;
+    details: FormArray<
+      FormGroup<{
+        quantity: FormControl;
+        amount: FormControl;
+        description: FormControl;
+      }>
+    >;
+  }>;
+
+  get details() {
+    return this.parent?.controls.details;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- projects/crm/src/app/invoice/invoice-form/invoice-form.component.ts
+++ projects/crm/src/app/invoice/invoice-form/invoice-form.component.ts
@@ -19,84 +19,13 @@ import {
       <hr />
 
       <h3>Détails de la facture</h3>
-      <div class="alert bg-warning text-white" *ngIf="details.length === 0">
-        <p>Vous devez ajouter des détails à votre facture</p>
-        <button class="btn btn-sm btn-outline-light" (click)="onAddDetails()">
-          + Ajouter ma première ligne
-        </button>
-      </div>
-      <section formArrayName="details">
-        <div
-          class="detail-row"
-          *ngFor="let group of details.controls; let i = index"
-          [formGroup]="group"
-        >
-          <div class="row mb-3">
-            <div class="col-7">
-              <input
-                formControlName="description"
-                [class.is-invalid]="
-                  group.controls.description.touched &&
-                  group.controls.description.invalid
-                "
-                name="description_{{ i }}"
-                id="description_{{ i }}"
-                type="text"
-                placeholder="Description"
-                class="form-control"
-              />
-              <p class="invalid-feedback">
-                La description est obligatoire et doit faire au moins 5
-                caractères !
-              </p>
-            </div>
-            <div class="col-2">
-              <input
-                formControlName="amount"
-                [class.is-invalid]="
-                  group.controls.amount.touched && group.controls.amount.invalid
-                "
-                name="amount_{{ i }}"
-                id="amount_{{ i }}"
-                type="number"
-                placeholder="Montant"
-                class="form-control"
-              />
-              <p class="invalid-feedback">Le montant est obligatoire</p>
-            </div>
-            <div class="col-2">
-              <input
-                formControlName="quantity"
-                [class.is-invalid]="
-                  group.controls.quantity.touched &&
-                  group.controls.quantity.invalid
-                "
-                type="number"
-                placeholder="Quantité"
-                class="form-control"
-              />
-              <p class="invalid-feedback">La quantité est obligatoire</p>
-            </div>
-            <div class="col-1">
-              <button
-                type="button"
-                class="btn w-auto d-block btn-sm btn-danger"
-                (click)="onRemoveDetails(i)"
-              >
-                X
-              </button>
-            </div>
-          </div>
-        </div>
-        <button
-          *ngIf="details.length > 0"
-          class="btn btn-primary btn-sm"
-          type="button"
-          (click)="onAddDetails()"
-        >
-          + Ajouter une ligne
-        </button>
-      </section>
+
+      <app-invoice-form-details 
+        (details-added)="onAddDetails()" 
+        (details-removded)="onRemoveDetails($event)" 
+        [parent]="invoiceForm"
+      ></app-invoice-form-details>
+
 
       <hr />
 
