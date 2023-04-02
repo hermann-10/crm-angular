3cda2a168415ebd2faf373703c2b7d34143f0677
Hermann Manuel <hermann.manuel@hotmail.com.com>
2023-04-02 13:50:42 +0200

    Implementation invoiceService

--- projects/crm/src/app/invoice/invoice.service.ts
+++ projects/crm/src/app/invoice/invoice.service.ts
@@ -1,55 +1,49 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, switchMap, tap } from "rxjs";
-import { environment } from "../../environments/environment";
-import { AuthService } from "../auth/auth.service";
-import { Invoice } from "./invoice";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, switchMap, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthService } from '../auth/auth.service';
+import { Invoice } from './invoice';
 
 const API_URL = environment.API_URL;
 
 @Injectable()
-export class invoiceService {
-    constructor(private http: HttpClient, private auth: AuthService){}
+export class InvoiceService {
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-    create(invoiceData: Invoice){
-        return this.auth.authToken.pipe(
-          tap((token) => {
-            if (!token) {
-              throw new Error('Unauthenticated');
-            }
-          }),
-          switchMap((token) => {
-            return this.http.post<Invoice>(
-              API_URL + '/invoice',
-              invoiceData,
-              {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
-            );
-          })
-        );
-    }
-
-    update(invoiceData: Invoice){
-        return this.http.put<Invoice>(
-          API_URL + '/invoice/' + invoiceData.id, invoiceData
-        );
-    }
-
-    delete(id: number){
-        return this.http.delete(
-          API_URL + '/invoice/'+ id)   
+  create(invoiceData: Invoice) {
+    return this.auth.authStatus$.pipe(
+      tap((token) => {
+        if (!token) {
+          throw new Error('Unauthenticated');
         }
+      }),
+      switchMap((token) => {
+        return this.http.post<Invoice>(API_URL + '/invoice', invoiceData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      })
+    );
+  }
 
-        findAll(): Observable<Invoice[]> {
-          return this.http.get<Invoice[]>(API_URL + '/invoice');
-        }
+  update(invoiceData: Invoice) {
+    return this.http.put<Invoice>(
+      API_URL + '/invoice/' + invoiceData.id,
+      invoiceData
+    );
+  }
 
-        find(id: number){
-            return this.http.get(
-              API_URL + '/invoice/'+ id
-            );
-        }
+  delete(id: number) {
+    return this.http.delete(API_URL + '/invoice/' + id);
+  }
+
+  findAll(): Observable<Invoice[]> {
+    return this.http.get<Invoice[]>(API_URL + '/invoice');
+  }
+
+  find(id: number) {
+    return this.http.get(API_URL + '/invoice/' + id);
+  }
 }
